7f96b565b2ecc93086f7cb613e62023da60cf64c
BTKbe <90573740+SystemCallBeam@users.noreply.github.com>
2022-08-13 16:08:08 +0700

    Add files via upload

---
+++ Tasks0008.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Tasks0008 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int s = scanner.nextInt();
+        System.out.println(2*s-x);
+    }
+}
